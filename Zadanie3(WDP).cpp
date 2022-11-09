a88fbaaa72d984fc79550b27a9462f20c7030cc2
Maksym Fedorets <Tifer_Aiden@protonmail.com>
2022-11-09 19:35:01 +0100

    1.0 ver

--- Zadanie3(WDP).cpp
+++ Zadanie3(WDP).cpp
@@ -1,11 +1,17 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
 #include <cmath>
 #include <string> 
 
 using namespace std;
 
 int x = 10, y = 2, z{};
+char operation, question[] = "suma", question2[4], answer[30];
 
+/// <summary>
+/// wykonać kilka operacji na x i y
+/// </summary>
 void PerformNumOperations() {
     printf("%d do potegi %d = %g\n", x, y, pow(x, y));
     printf("pierwiastek z %d = %g\n", x, sqrt(x));
@@ -29,16 +35,16 @@ void SummXAndY() {
 void GetOperation(int a, int b, char operation) {
     switch (operation) {
     case '+':
-        printf("suma: %d + %d = %d\n\n", x, y, z = x + y);
+        printf("suma: %d + %d = %d\n", x, y, z = x + y);
         break;
     case '*':
-        printf("iloczyn: %d * %d = %d\n\n", x, y, z = x * y);
+        printf("iloczyn: %d * %d = %d\n", x, y, z = x * y);
         break;
     case '/':
-        printf("iloraz: %d / %d = %d\n\n", x, y, z = x / y);
+        printf("iloraz: %d / %d = %d\n", x, y, z = x / y);
         break;
     case '-':
-        printf("roznica: %d - %d = %d\n\n", x, y, z = x - y);
+        printf("roznica: %d - %d = %d\n", x, y, z = x - y);
         break;
     default: 
         cout << "Nieznana operacja\n\n";
@@ -48,21 +54,26 @@ void GetOperation(int a, int b, char operation) {
 
 int main()
 {
-    char operation;
-    char question[] = "suma";
-    char answer[10] ;
-
+    // wywołać kilka funkcji, aby je sprawdzić
     SummXAndY();
     PerformNumOperations();
 
+    // wykonać wybraną przez użytkownika operację na x i y
     cout << "Podaj operacje (-, +, /, *): ";
     cin >> operation;
     GetOperation(x, y, operation);
 
+    // zagadka, w której pyta się o prawidłowe działanie
+    char partOfQuestion2[1024] = "jaka operacje nalezy wykonac n",
+        partOfQuestion3[1024] = "a 2 i 6, aby otrzymac 8?";
+    strcpy(question2, question);
+    strcat(partOfQuestion2, partOfQuestion3);
+
+    // powtarzać pytanie do momentu udzielenia prawidłowej odpowiedzi
     do {
-        cout << "jaka operacje nalezy wykonac na 2 i 6, aby otrzymac 8?\n";
+        printf("\nodpowiedz sklada sie z %I64u leter\n%s\n", strlen(question), partOfQuestion2);
         cin >> answer;
-    } while (strcmp(question, answer) != 0);
+    } while (strcmp(question2, answer) != 0);
 
     cout << "prawidlowa odpowiedz!\n";
 
